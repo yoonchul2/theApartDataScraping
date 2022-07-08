6d5b6d3ca8986ec99e27c01d609c45415f8c79f5
mrsbok <swingebk@gmail.com>
2022-07-08 17:24:55 +0900

    배치모듈 셋팅

---
+++ realDealBatch/src/main/kotlin/com/realdealbatch/RealDealBatchApplication.kt
@@ -0,0 +1,11 @@
+package com.realdealbatch
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RealDealBatchApplication
+
+fun main(args: Array<String>) {
+    runApplication<RealDealBatchApplication>(*args)
+}
---
+++ realDealBatch/src/test/kotlin/com/realdealbatch/RealDealBatchApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.realdealbatch
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RealDealBatchApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
