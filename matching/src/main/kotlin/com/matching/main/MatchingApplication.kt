f9f4beb2612adf4e9f6e72fecdd47fecbde7db60
신세벽 <soulblade.mk2@gmail.com>
2022-07-12 13:59:41 +0900

    yml 파일 추가

--- matching/src/main/kotlin/com/matching/main/MatchingApplication.kt
+++ matching/src/main/kotlin/com/matching/main/MatchingApplication.kt
@@ -2,7 +2,13 @@ package com.matching.main
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.boot.web.servlet.ServletComponentScan
+import org.springframework.context.annotation.EnableAspectJAutoProxy
+import org.springframework.scheduling.annotation.EnableScheduling
 
+@ServletComponentScan
+@EnableScheduling
+@EnableAspectJAutoProxy
 @SpringBootApplication
 class MatchingApplication
 
